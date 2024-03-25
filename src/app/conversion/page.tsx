37b4144f90c9872529f24b79f6a43ef1cce1972d
Slipromise <chengchun05@gmail.com>
2024-03-26 01:52:23 +0800

    TASK-444 初始化專案

---
+++ src/app/conversion/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function Conversion({}: Props) {
+  return <div>Conversion</div>;
+}
+
+export default Conversion;
---
+++ src/app/selection/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function Selection({}: Props) {
+  return <div>Selection</div>;
+}
+
+export default Selection;
